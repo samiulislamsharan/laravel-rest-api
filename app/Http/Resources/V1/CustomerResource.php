defb1bba1c04cc6ff222dce7d3dd9b454ffca726
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-21 01:06:07 +0600

    Add 'invoice' field to CustomerResource toArray method

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -23,6 +23,7 @@ class CustomerResource extends JsonResource
             'city' => $this->city,
             'state' => $this->state,
             'postalCode' => $this->postal_code,
+            'invoice' => new InvoiceCollection($this->whenLoaded('invoices')),
         ];
     }
 }
