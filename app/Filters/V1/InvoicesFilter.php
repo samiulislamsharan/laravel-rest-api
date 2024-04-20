029544110f2be808bed9075a56e52d647f640062
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-20 17:48:53 +0600

    Add InvoicesFilter class for handling invoice queries

---
+++ app/Filters/V1/InvoicesFilter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filters\V1;
+
+use App\Filter\ApiFilter;
+use Illuminate\Http\Request;
+
+class InvoicesFilter extends ApiFilter
+{
+    protected $allowedParams = [
+        'name' => ['eq'],
+        'type' => ['eq'],
+        'email' => ['eq'],
+        'address' => ['eq'],
+        'city' => ['eq'],
+        'state' => ['eq'],
+        'postalCode' => ['eq', 'gt', 'lt'],
+    ];
+
+    protected $columnMap = [
+        'postalCode' => 'postal_code',
+    ];
+
+    protected $operatorMap = [
+        'eq' => '=',
+        'gt' => '>',
+        'gte' => '>=',
+        'lt' => '<',
+        'lte' => '<=',
+    ];
+
+    public function transform(Request $request)
+    {
+        $eloquentQuery = [];
+
+        foreach ($this->allowedParams as $param => $operators) {
+            $query = $request->query($param);
+
+            if (!isset($query)) {
+                continue;
+            }
+
+            $column = $this->columnMap[$param] ?? $param;
+
+            foreach ($operators as $operator) {
+                if (isset($query[$operator])) {
+                    $eloquentQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
+                }
+            }
+        }
+
+        return $eloquentQuery;
+    }
+}
