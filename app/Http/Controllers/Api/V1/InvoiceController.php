87625ec726d7c73cd74251384ef2806c4decb70b
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-21 01:06:22 +0600

    Refactor InvoiceController.php to update namespace and import statements

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Filters\V1\InvoicesFilter;
+use Illuminate\Http\Request;
+
 use App\Models\Invoice;
-use App\Http\Requests\StoreInvoiceRequest;
-use App\Http\Requests\UpdateInvoiceRequest;
+use App\Filters\V1\InvoicesFilter;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\StoreInvoiceRequest;
+use App\Http\Requests\V1\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Resources\V1\InvoiceCollection;
-use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
