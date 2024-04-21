218bf6cecdac0077368ffd4778dc7dbbfbc14738
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-21 11:23:29 +0600

    Refactor InvoiceController.php to add bulkStore method

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Invoice;
 use App\Filters\V1\InvoicesFilter;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\V1\StoreInvoiceRequest;
+use App\Http\Requests\V1\BulkStoreInvoiceRequest;
 use App\Http\Requests\V1\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceResource;
 use App\Http\Resources\V1\InvoiceCollection;
+use Illuminate\Support\Arr;
 
 class InvoiceController extends Controller
 {
@@ -58,9 +59,19 @@ class InvoiceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreInvoiceRequest $request)
+    public function store(Request $request)
     {
         //
+    }
+
+    public function bulkStore(BulkStoreInvoiceRequest $request)
+    {
+        $bulk= collect($request->all())->map(function($arr, $key)
+        {
+            return Arr::except($arr, ['customerId', 'billedDate', 'paidDate']);
+        });
+
+        Invoice::insert($bulk->toArray());
     }
 
     /**
