eced7ab1acec22d9238c70aaa87c75f3dea11f31
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-21 11:10:00 +0600

    Update CustomerController@update method to use UpdateCustomerRequest for validation and return updated customer resource

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -76,7 +76,9 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
+
+        return new CustomerResource($customer);
     }
 
     /**
